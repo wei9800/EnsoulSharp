c8369687aade27e8baead67799b6d2e3e724a691
samuelchow1997 <samuelchow1997@gmail.com>
2019-12-01 23:49:47 +0800

    senna
    senna release

---
+++ 14Senna/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace _14Senna
+{
+    using System;
+    using EnsoulSharp.SDK;
+    using EnsoulSharp;
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            GameEvent.OnGameLoad += OnGameLoad;
+
+
+        }
+
+        static void OnGameLoad()
+        {
+            Game.Print("14 Senna Load");
+            Senna.OnLoad();
+        }
+    }
+}

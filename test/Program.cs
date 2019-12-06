ad9aabf2c609bd4d64bfce451c7b67a57d2a8781
samuelchow1997 <samuelchow1997@gmail.com>
2019-12-07 01:36:48 +0800

    say
    say

---
+++ test/Program.cs
@@ -0,0 +1,42 @@
+﻿
+using System;
+
+using System.Linq;
+
+using System.Collections.Generic;
+using EnsoulSharp;
+using EnsoulSharp.SDK;
+using EnsoulSharp.SDK.MenuUI;
+using EnsoulSharp.SDK.MenuUI.Values;
+using SharpDX;
+
+namespace test
+{
+    class Program
+    {
+        private static Menu  DrawMenu, Config;
+        static void Main(string[] args)
+        {
+            GameEvent.OnGameLoad += OnGameLoad;
+        }
+
+        static void OnGameLoad()
+        {
+            Config = new Menu("123", "Chat", true);
+            DrawMenu = new Menu("Chat", "Chat");
+            DrawMenu.Add(new MenuKeyBind("say", "Say", System.Windows.Forms.Keys.T, KeyBindType.Press));
+            Config.Add(DrawMenu);
+            Config.Attach();
+
+            Game.OnUpdate += OnGameUpdate;
+        }
+
+        static void OnGameUpdate(EventArgs args)
+        {
+            if (DrawMenu["say"].GetValue<MenuKeyBind>().Active)
+            {
+                Game.Say("123",false);
+            }
+        }
+    }
+}
